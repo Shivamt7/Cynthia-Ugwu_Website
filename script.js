5bc9d6e8395d9749bc08e7914ccc198482daae6e
Shivamt7 <shivam.s.mishra7@gmail.com>
2025-07-13 17:27:05 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+function firstPageAnimation() {
+  var tl = gsap.timeline();
+
+  tl.from(".nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+
+    .to(".boundingElem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
+    })
+
+    .from(".herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (details) {
+    document.querySelector(
+      ".miniCircle"
+    ).style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+  });
+}
+
+circleMouseFollower();
+firstPageAnimation();
